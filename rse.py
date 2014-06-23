32e3a6eccfa0a69d5cb6956020708fe272cb42be
root <root@rucio-testbed-server.grid.sinica.edu.tw>
2014-06-23 11:46:14 +0000

    delete stuff

--- rse.py
+++
@@ -1,6 +0,0 @@
-from rucio.client.rseclient import RSEClient
-rse = RSEClient()
-
-print rse.list_rse_attributes('TW-EOS00_AMS02DATADISK')
-
-
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-#r = requests.get('https://rucio-front03.grid.sinica.edu.tw/ping:443')
-r = requests.get('https://rucio-testbed-server.grid.sinica.edu.tw/ping:443', headers = None, verify='/etc/grid-security/certificates/ASGCCA-2007.pem')
-print r
