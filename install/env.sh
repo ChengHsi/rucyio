9663a31211d7e7b7d3c9251e711bcc31153421e3
root <root@chenghsi-rucio-server2.cern.ch>
2014-06-23 17:34:15 +0200

    changed some installation stuff

--- install/env.sh
+++ install/env.sh
@@ -4,12 +4,24 @@ easy_install virtualenv
 pip install virtualenvwrapper
 easy_install python-setuptools
 #easy_install --upgrade
-#curl -O http://python-distribute.org/distribute_setup.py
-#sudo python distribute_setup.py
-#sudo rm distribute_setup.py
+curl -O http://python-distribute.org/distribute_setup.py
+sudo python distribute_setup.py
+sudo rm distribute_setup.py
 yum install -y git
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-source /usr/bin/virtualenvwrapper.sh
+yum install -y gcc.x86_64
+#yum install krb5-config
+yum install -y krb5-devel.x86_64
+yum install -y krb5-workstation.x86_64
+yum install -y python-devel
+yum install -y mysql-server-5.1.73-3.el6_5.x86_64
+yum install -y mysql-libs-5.1.73-3.el6_5.x86_64
+yum install -y mysql-5.1.73-3.el6_5.x86_64
+yum install -y mysql-devel-5.1.73-3.el6_5.x86_64
+service mysqld start
+mysqladmin -u root password asgcddm
+mysqladmin -uroot -p asgcddm create rucio
+#export WORKON_HOME=$HOME/.virtualenvs
+#export PROJECT_HOME=$HOME/Devel
+#source /usr/bin/virtualenvwrapper.sh
 #workon
-mkvirtualenv rucio
+#mkvirtualenv rucio
--- install/rucio-pip.py
+++ install/rucio-pip.py
@@ -11,6 +11,18 @@ with open("/opt/rucio/tools/pip-requires") as f:
             print line[0]
             subprocess.call(["pip", "install", line[0]])
 
-install_list=["argcomplete==0.8.0", 'dogpile.cache==0.5.3', 'kerberos==1.1.1', 'requests-kerberos==0.4.0', 'wsgiref==0.1.2', 'urllib3==1.7.1', 'requests==2.2.1', 'distribute==0.7.3']
-for i in install_list:
+pip_list=["argcomplete==0.8.0", 'dogpile.cache==0.5.3', 'kerberos==1.1.1', 'requests-kerberos==0.4.0', 'wsgiref==0.1.2', 'urllib3==1.7.1', 'requests==2.2.1', 'distribute==0.7.3', "kerberos==1.1.1", "requests-kerberos==0.4.0", "MySQL-python"]
+easy_list = ["PIL==1.1.7"]
+
+for i in pip_list:
     subprocess.call(["pip", "install", i])
+
+for i in easy_list:
+    subprocess.call(["easy_install", i])
+
+subprocess.call(["wget", "http://pysftp.googlecode.com/files/pysftp-0.2.2.tar.gz"])
+subprocess.call(["pip", "install", "pysftp-0.2.2.tar.gz"])
+subprocess.call(["rm", "-rf", "pysftp-0.2.2.tar.gz"])
+
+
+
