a60bed1a9e77ca5db7ad3d29d31da9412d28531e
ChengHsi <chenghsi.chao@twgrid.org>
2014-07-14 13:38:32 +0000

    add weijen_register, edit git_ssh for general use

--- install/git_ssh.py
+++ install/git_ssh.py
@@ -1,14 +1,22 @@
-import subprocess
-sub = subprocess.Popen(["find", "/", "-name", ".git"], stdout=subprocess.PIPE)
+import subprocess, os
+sub = subprocess.Popen(["find", os.environ['HOME'], "-name", ".git"], stdout=subprocess.PIPE)
 sub = sub.communicate()
 old_string = 'https://github.com/'
 new_string = 'ssh://git@github.com/'
 for x in sub[0].split('\n')[:-1]:
-    x = x + '/config'
-    s = open(x).read()
-    if old_string in s:
-        s = s.replace(old_string, new_string)
-        f = open(x, 'w')
-        f.write(s)
-        f.flush()
-        f.close()
+    print x
+    prev_dir = os.getcwd()
+    os.chdir(x)
+    #pwd = subprocess.Popen('pwd', stdout=subprocess.PIPE)
+    test = subprocess.Popen(["git", "remote", "show", "origin"], stdout=subprocess.PIPE)
+    if 'ChengHsi' in test.communicate()[0]:
+        x = x + '/config'
+        s = open(x).read()
+        if old_string in s:
+            print 'replacing',old_string, 'in', x, 'with', new_string
+            s = s.replace(old_string, new_string)
+            f = open(x, 'w')
+            f.write(s)
+            f.flush()
+            f.close()
+    os.chdir(prev_dir)
