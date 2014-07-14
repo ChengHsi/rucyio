f2f8843c1949980de2751ce65608cead45c1c807
root <root@rucio-testbed-server.grid.sinica.edu.tw>
2014-07-14 19:22:33 +0000

    update test/replica

--- test/replica.py
+++ test/replica.py
@@ -3,21 +3,22 @@ from rucio.client.replicaclient import ReplicaClient
 rep = ReplicaClient()
 scope = ''
 filename = 'wj_test02'
+scope = 'twgrid-user-testuser1'
 print 'before:'
-for x in rep.list_replicas([{'scope': 'twgrid-user-testuser1', 'name': filename}]):
+for x in rep.list_replicas([{'scope': scope, 'name': filename}]):
     print x
-rep.delete_replicas('TW-EOS00_AMS02DATADISK', [{'scope': 'twgrid-user-testuser1', 'name': filename}])
+rep.delete_replicas('TW-EOS00_AMS02DATADISK', [{'scope': scope, 'name': filename}])
 print 'after deletion:'
-for x in rep.list_replicas([{'scope': 'twgrid-user-testuser1', 'name': filename}]):
+for x in rep.list_replicas([{'scope': scope, 'name': filename}]):
     print x
-#rep.add_replica('TW-EOS00_AMS02DATADISK', 'twgrid-user-testuser1', filename, 15, '230003b2', pfn='/eos/ams/amsdatadisk/twgrid-user-testuser1/87/1f/wj_test02')
+#rep.add_replica('TW-EOS00_AMS02DATADISK', scope, filename, 15, '230003b2', pfn='/eos/ams/amsdatadisk/twgrid-user-testuser1/87/1f/wj_test02')
 #print 'after add:'
-#for x in rep.list_replicas([{'scope': 'twgrid-user-testuser1', 'name': filename}]):
+#for x in rep.list_replicas([{'scope': scope, 'name': filename}]):
 #    print x
 ###################
 #from rucio.rse import rsemanager as rsemgr
 #from rucio.core import replica
-#for x in replica.list_replicas([{'scope': 'twgrid-user-testuser1', 'name': filename}]):
+#for x in replica.list_replicas([{'scope': scope, 'name': filename}]):
 #    print x
 ###################
 #from rucio.rse import rsemanager as rsemgr
@@ -35,4 +36,4 @@ for x in rep.list_replicas([{'scope': 'twgrid-user-testuser1', 'name': filename}
 #
 #print rsemgr.get_rse_info('RUCIO-DPM01_TWGRIDSCRATCHDISK')i
 #from rucio.api.replica import add_replicas as add
-#add('TW-EOS00_AMS02DATADISK', [{'scope': 'twgrid-user-testuser1', 'name': filename}], 'root')
+#add('TW-EOS00_AMS02DATADISK', [{'scope': scope, 'name': filename}], 'root')
