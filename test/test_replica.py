aeb10b3f874ab4cdc66bc2f84e2668b249e343d3
Cheng-Hsi Chao <curiojustus@gmail.com>
2015-01-09 08:02:00 +0000

    Add test changes

--- test/test_replica.py
+++ test/test_replica.py
@@ -1,7 +1,11 @@
 from rucio.client.didclient import DIDClient
 from rucio.client.replicaclient import ReplicaClient
-
+import sys
+import pdb; pdb.set_trace()
 rep = ReplicaClient()
-
-rep.list_replicas([{'scope':'ams-2011B-ISS.B620-pass4', 'name':'1340252898.00981893.root'}])
+scope = sys.argv[1]
+name = sys.argv[2]
+# rep.list_replicas([{'scope':'ams-2011B-ISS.B620-pass4', 'name':'1340252898.00981893.root'}])
+for x in rep.list_replicas([{'scope':scope, 'name':name}]):
+    print x
 
