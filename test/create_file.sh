83458411099233c67c62ef45cacdc85345e87211
chenghsi <chenghsi@pb-d-128-141-187-126.cern.ch>
2015-06-22 16:17:57 +0200

    update create_file.sh

--- test/create_file.sh
+++ test/create_file.sh
@@ -1,4 +1,6 @@
-TEMP_DIR=/root/chchao/rucyio/tmp
+rucyio=`find ~/ -name rucyio 2>&1 | grep -v 'Permission denied'`
+TEMP_DIR=$rucyio/tmp
+mkdir -p $TEMP_DIR
 now=$(date +"%Y%m%dT%H%M%S")
 TESTFILE=$TEMP_DIR/file$now
 
