acda2f1342bf1326a32651241d7dc153ec622c74
Cheng-Hsi Chao <curiojustus@gmail.com>
2016-02-01 05:52:54 +0000

    Add test_rename to test_client.py

--- test/test_client.py
+++ test/test_client.py
@@ -5,13 +5,23 @@ client = Client()
 # scope = sys.argv[1]
 # name = sys.argv[2]
 # scope = 'ams-2011B-ISS.B620-pass4'
-# name = '1340252898.00981893.root'
+# name = 'file20150708T164717'
 scope = 'ams-user-chenghsi'
-name = 'file20150721T110241'
+# name = sys.argv[1]
+name = 'Old_dataset'
+new_name = 'NewNew_dataset'
 # client.list_replicas([{'scope':'ams-2011B-ISS.B620-pass4', 'name':'1340252898.00981893.root'}])
-for x in client.list_replicas([{'scope':scope, 'name':name}]):
-    print x
+# for x in client.list_replicas([{'scope':scope, 'name':name}]):
+#     print x
+
+# client.set_metadata(scope=scope, name=name, key='lifetime', value=0)
 
 # print client.list_scopes()
 # for x in client.scope_list('ams-user-chenghsi'):
 #     print x
+
+
+def test_rename(scope, name, new_name, type):
+    client.rename_did(scope, name, new_name, type)
+
+test_rename(scope, name, new_name, 'dataset')
